ee33b35bca498618ffb183300e9bf95356fb1de4
Lau <laurabedoyacoronado@gmail.com>
2025-07-25 11:41:39 -0500

    organizacion js

--- app/static/funciones.js
+++ app/static/funciones.js
@@ -1,3 +1,17 @@
+const userIcon = document.getElementById('user-icon');
+        const userDropdown = document.getElementById('user-dropdown');
+
+        userIcon.addEventListener('click', () => {
+            userDropdown.classList.toggle('active');
+        });
+
+        // Cerrar el menú si se hace clic fuera de él
+        window.addEventListener('click', (event) => {
+            if (!userIcon.contains(event.target) && !userDropdown.contains(event.target)) {
+                userDropdown.classList.remove('active');
+            }
+        });
+
 document.getElementById('checkin').addEventListener('click', () => {
     document.getElementById('checkin').showPicker();
 });
