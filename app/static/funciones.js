20c3f7eaa51edf7002ba5e03c0cbdf1e0953448b
Lau <laurabedoyacoronado@gmail.com>
2025-07-25 11:36:46 -0500

    Arreglos en el buscador

---
+++ app/static/funciones.js
@@ -0,0 +1,7 @@
+document.getElementById('checkin').addEventListener('click', () => {
+    document.getElementById('checkin').showPicker();
+});
+
+document.getElementById('checkout').addEventListener('click', () => {
+    document.getElementById('checkout').showPicker();
+});
